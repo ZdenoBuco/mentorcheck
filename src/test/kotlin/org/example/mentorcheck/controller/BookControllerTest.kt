ade9fff98abf112a8ffdc98bdcc6e35d0897473d
Zdenko Bučo <zdeno.buco@applifting.cz>
2025-12-15 10:51:37 +0100

    remove failing tests

--- src/test/kotlin/org/example/mentorcheck/controller/BookControllerTest.kt
+++ src/test/kotlin/org/example/mentorcheck/controller/BookControllerTest.kt
@@ -76,7 +76,6 @@ class BookControllerTest {
 
         assertEquals(HttpStatus.OK, exchange.statusCode)
         assertEquals(update.title, exchange.body!!.title)
-        throw RuntimeException("test failed")
     }
 
     fun provideBooks(): Stream<Book> = Stream.of(
